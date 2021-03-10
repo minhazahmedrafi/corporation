34bf1613d01ff5e996f14707ad2786cbe4e9fb39
Minhaz Ahmed Rafi <40710320+minhazahmedrafi@users.noreply.github.com>
2021-03-10 12:05:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Counter Animation
+let nCount = function (selector) {
+    $(selector).each(function () {
+      $(this).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 4000,
+        easing: "swing",
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+    });
+  }
+  
+  let a = 0
+  $(window).scroll(function () {
+    let oTop = $(".single-carrier").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+      a++;
+      nCount(".rect>h1")
+    }
+  });
